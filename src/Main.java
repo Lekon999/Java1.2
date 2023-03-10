4cf31b68219679ddb486e5571bcd222ee4585d3d
Кочнев Илья <Bankir369@gmail.com>
2023-03-10 22:43:10 +0300

    Inicial commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         int balance = 150;
         int payment = 1200;
         int bonus = balance + payment;
-        if (payment > 999) {bonus = balance + payment + payment / 100;
-        }
+        if (payment > 999)
+            bonus = balance + payment + payment / 100;
         System.out.println(bonus);
     }
 }
